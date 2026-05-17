1047556646009fe02760f2b818d1c5a8f8caf4e5
Ushan Bartaula <ushanbartaula1994@gmail.com>
2026-05-17 22:27:33 +0545

    fixed logout bugs

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,16 +5,16 @@ import {User} from '../models/user.model.js'
 
 
 
-export const verifyJWT=asyncHandler(async(req,resizeBy,next)=>{
+export const verifyJWT=asyncHandler(async(req,res,next)=>{
   try {
     const token =
-      req.cookie?.accessToken ||
+      req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
     if (!token) {
       throw new ApiError(401, "unauthorised error");
     }
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    const user = await User.findById(decodedToken?._Id).select(
+    const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken"
     );
     if (!user) {
